1955b047d42223c929de7342eb8611e8ad136a21
Aliaksei Loi <aliaksei.loi@itechart-group.com>
2020-01-07 09:29:49 +0300

    update for page

---
+++ src/modules/Galery/images/index.js
@@ -0,0 +1,175 @@
+import image1 from "./766A8806.jpg";
+import image2 from "./766A8860.jpg";
+import image3 from "./766A8874.jpg";
+import image4 from "./Б1-28.jpg";
+import image5 from "./Б1-60.jpg";
+import image6 from "./Б1-63.jpg";
+import image7 from "./Б2-142.jpg";
+import image8 from "./Б2-180.jpg";
+import image9 from "./Б3-74.jpg";
+import image10 from "./Б3-96.jpg";
+import image11 from "./Б3-113.jpg";
+import image12 from "./Б3-114.jpg";
+import image13 from "./Б3-194.jpg";
+import image14 from "./i (90).JPG";
+import image15 from "./i (175).JPG";
+import image16 from "./i (349).JPG";
+import image17 from "./i (409).JPG";
+import image18 from "./i (428).JPG";
+import image19 from "./i (532).JPG";
+import image20 from "./IMG_0330.JPG";
+import image21 from "./IMG_0334.JPG";
+import image22 from "./IMG_0335.JPG";
+import image23 from "./IMG_0336.JPG";
+import image24 from "./IMG_0338.JPG";
+import image25 from "./IMG_0339.JPG";
+import image26 from "./IMG_0340.JPG";
+import image27 from "./IMG_0342.JPG";
+import image28 from "./IMG_0344.JPG";
+import image29 from "./IMG_0346.JPG";
+import image30 from "./M_D - 066.jpg";
+import image31 from "./M_D - 073.jpg";
+import image32 from "./M_D - 086.jpg";
+import image33 from "./M_D - 106.jpg";
+import image34 from "./M_D - 130.jpg";
+import image35 from "./M_D - 139.jpg";
+import image36 from "./M_D - 159.jpg";
+import image37 from "./M_D - 162.jpg";
+import image38 from "./M_D - 190.jpg";
+import image39 from "./M_D - 253.jpg";
+import image40 from "./M_D - 311.jpg";
+import image41 from "./M_D - 321.jpg";
+import image42 from "./M_D - 370.jpg";
+import image43 from "./M_D - 425.jpg";
+import image44 from "./M_D - 448.jpg";
+import image45 from "./M_D - 461.jpg";
+import image46 from "./M_D - 489.jpg";
+import image47 from "./M_D - 542.JPG";
+import image48 from "./M_D - 661.jpg";
+import image49 from "./M_D - 663.jpg";
+import image50 from "./M_D - 667.jpg";
+import image51 from "./M_D - 692.jpg";
+import image52 from "./M_D - 713.jpg";
+import image53 from "./M_D - 749.jpg";
+import image54 from "./M_D - 791.jpg";
+import image55 from "./sascheka_051.jpg";
+import image56 from "./sascheka_052.jpg";
+import image57 from "./sascheka_083.jpg";
+import image58 from "./sascheka_085.jpg";
+import image59 from "./sascheka_134.jpg";
+import image60 from "./sascheka_149.jpg";
+import image61 from "./sascheka_153.jpg";
+import image62 from "./sascheka_192.jpg";
+import image63 from "./sascheka_196.jpg";
+import image64 from "./sascheka_212.jpg";
+import image65 from "./sascheka_282.jpg";
+import image66 from "./sascheka_336.jpg";
+import image67 from "./sascheka_443.jpg";
+import image68 from "./sascheka_451.jpg";
+import image69 from "./sascheka_504.jpg";
+import image70 from "./sascheka_538.jpg";
+import image71 from "./sascheka_570.jpg";
+import image72 from "./sascheka_572.jpg";
+import image73 from "./wedding day-20.jpg";
+import image74 from "./wedding day-131.jpg";
+import image75 from "./wedding day-148.jpg";
+import image76 from "./wedding day-149.jpg";
+import image77 from "./wedding day-187.jpg";
+import image78 from "./wedding day-219.jpg";
+import image79 from "./wedding day-231.jpg";
+import image80 from "./Wedding day35.jpg";
+import image81 from "./Wedding day51.jpg";
+import image82 from "./Wedding day78.jpg";
+import image83 from "./Wedding day138.jpg";
+import image84 from "./Wedding day149.jpg";
+import image85 from "./Wedding day300.jpg";
+import image86 from "./Wedding day355.jpg";
+
+export const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+  image32,
+  image33,
+  image34,
+  image35,
+  image36,
+  image37,
+  image38,
+  image39,
+  image40,
+  image41,
+  image42,
+  image43,
+  image44,
+  image45,
+  image46,
+  image47,
+  image48,
+  image49,
+  image50,
+  image51,
+  image52,
+  image53,
+  image54,
+  image55,
+  image56,
+  image57,
+  image58,
+  image59,
+  image60,
+  image61,
+  image62,
+  image63,
+  image64,
+  image65,
+  image66,
+  image67,
+  image68,
+  image69,
+  image70,
+  image71,
+  image72,
+  image73,
+  image74,
+  image75,
+  image76,
+  image77,
+  image78,
+  image79,
+  image80,
+  image81,
+  image82,
+  image83,
+  image84,
+  image85,
+  image86
+];
--- src/modules/Galery/index.js
+++ src/modules/Galery/index.js
@@ -1,19 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-import { Slider } from "./Slider";
-import { Category } from "./Category";
+import Fade from "react-reveal/Fade";
 
-import { mocks } from "./mocks";
+import FsLightbox from "fslightbox-react";
+
+import { images } from "./images";
+
+import "./index.scss";
+
+// console.log(images);
 
 export const Galery = () => {
+  const [{ toggler, current }, setState] = useState({
+    toggler: false,
+    current: 1
+  });
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const toggle = current => {
+    setState(state => ({
+      toggler: !state.toggler,
+      current
+    }));
+  };
+
   return (
     <>
-      <Slider content={mocks} />
-      <Category />
+      <div className="category">
+        {images.map((item, i) => (
+          <Fade key={i}>
+            <img
+              onClick={() => toggle(i + 1)}
+              src={item}
+              alt="superimg"
+              className="category-image"
+            />
+          </Fade>
+        ))}
+      </div>
+      <FsLightbox slide={current} toggler={toggler} sources={images} />
     </>
   );
 };
